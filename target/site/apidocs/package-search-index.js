5849bbe53e539a58072f18282dcf486c7150612e
Zendia <discitevivere0@gmail.com>
2024-03-10 20:27:33 +0100

    Documentation generated (JAVADOC) and added project folders to the commit

---
+++ target/site/apidocs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.tes1.refactoring"}];updateSearchResults();
---
+++ target/site/apidocs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"com.tes1.refactoring","l":"Date"}];updateSearchResults();
